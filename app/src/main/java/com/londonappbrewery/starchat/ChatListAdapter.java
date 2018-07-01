9d9a111f1f490fb7a893087819b35c4faca5b007
aditishevale <aditi.shevale999@gmail.com>
2018-07-01 18:05:46 +0530

    NewClass ChatListAdapter

--- app/src/main/java/com/londonappbrewery/starchat/ChatListAdapter.java
+++ app/src/main/java/com/londonappbrewery/starchat/ChatListAdapter.java
@@ -1,11 +1,21 @@
 package com.londonappbrewery.starchat;
 
+import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 
-public class ChatListAdapter extends BaseAdapter {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
 
+import java.util.ArrayList;
+
+public class ChatListAdapter extends BaseAdapter {
+    private Activity mActivity;
+    private DatabaseReference mDatabaseReference;
+    private String mDisplayName;
+    private ArrayList<DataSnapshot> mSnapshotList;
 
     @Override
     public int getCount() {
